3fac2cc6f8f6da74cf229be61085d5fee6b7973e
cr & kl <student@epicodus.com>
2015-07-20 15:43:12 -0700

    send texts to multiple contacts.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+
+  root to: 'messages#index'
+
+  resources :contacts
   resources :messages, :only => [:index, :new, :create, :show]
+
 end
---
+++ db/migrate/20150720214122_contacts.rb
@@ -0,0 +1,8 @@
+class Contacts < ActiveRecord::Migration
+  def change
+    create_table :contacts do |t|
+      t.string "name"
+      t.string "phone_number"
+    end
+  end
+end
